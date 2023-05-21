0a4496d876374b2686b938eeaa1bc568719b0710
Matt Moreno <88680203+matt-moreno@users.noreply.github.com>
2023-05-21 12:50:03 -0700

    Conditionally render Questions component

--- quizzical-project/src/App.jsx
+++ quizzical-project/src/App.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect} from 'react'
 import Start from "./Start"
+import Questions from './Questions'
 
 function App() {
 
+  const [quiz, setQuiz] = useState(false)
+    
+    function startQuizBtn() {
+      // Make it flip the boolean later  
+      setQuiz(true)
+      getQuizData()
+    }
+
+    async function getQuizData() {
+        const res = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+        const quizData = await res.json()
+        return quizData
+    }
+
   return (
     <>
-      <Start />
+      {!quiz && <Start startQuiz={startQuizBtn}/>}
+      {quiz && <Questions />}
     </> 
   )
 }
