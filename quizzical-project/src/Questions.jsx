49905aff38b88e56575482492122ab694be6c756
Matt Moreno <88680203+matt-moreno@users.noreply.github.com>
2023-05-24 12:18:40 -0700

    get rid of the map inside component

--- quizzical-project/src/Questions.jsx
+++ quizzical-project/src/Questions.jsx
@@ -4,33 +4,27 @@ import uuid from 'react-uuid'
 import './App.css'
 
 function Questions(props) {
-    const triviaQuestions = props.data.map(trivia => {
+    // const triviaQuestions = props.data.map(trivia => {
         
-        const questionsArr = [trivia.correct_answer, 
-        trivia.incorrect_answers[0], 
-        trivia.incorrect_answers[1], 
-        trivia.incorrect_answers[2]]
+    //     const questionsArr = [trivia.correct_answer, 
+    //     trivia.incorrect_answers[0], 
+    //     trivia.incorrect_answers[1], 
+    //     trivia.incorrect_answers[2]]
         
-        const randomizedQuestions = questionsArr.sort((a, b) => 0.5 - Math.random())
+    //     const randomizedQuestions = questionsArr.sort((a, b) => 0.5 - Math.random())
         
-        console.log(randomizedQuestions)
-
-        return (
-        <div className="question-container" key={uuid()}>
-            <p>{decode(trivia.question)}</p>
-            <div className='answer-container'>
-                <button>{trivia.correct_answer}</button>
-                <button>{trivia.incorrect_answers[0]}</button>
-                <button>{trivia.incorrect_answers[1]}</button>
-                <button>{trivia.incorrect_answers[2]}</button>
+    //     console.log(randomizedQuestions)
+    console.log(props)
+    return (
+        <div className="question-container">
+            <p>{decode(props.question)}</p>
+            <div className='answer-container'>             
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
             </div>
         </div>
-        )
-    })
-    return (
-        <>
-            {triviaQuestions}
-        </>
     )
 }
 
