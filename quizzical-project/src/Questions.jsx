08d5a9b00d49207e8e1423a619391d6df1755671
Matt Moreno <88680203+matt-moreno@users.noreply.github.com>
2023-06-04 22:19:52 -0700

    Map over answers array and create button components

--- quizzical-project/src/Questions.jsx
+++ quizzical-project/src/Questions.jsx
@@ -1,24 +1,26 @@
 import { useState, useEffect } from 'react'
 import {decode} from 'html-entities';
 import uuid from 'react-uuid'
-import './App.css'
+import './index.css'
 
 function Questions(props) {
-    const questions = [
-        props.correctAnswer,
-        props.incorrectAnswer[0],
-        props.incorrectAnswer[1],
-        props.incorrectAnswer[2]
-    ].sort((a, b) => 0.5 - Math.random())
+
+    const answersElement = props.answers.map(answer => {
+        return (
+            <button
+            key={uuid()}
+            onClick={() => props.handleClickAnswer(props.id, answer)}
+            className="answer-btn">
+                {decode(answer)}
+            </button>
+        )
+    })
 
     return (
         <div className="question-container">
             <p>{decode(props.question)}</p>
             <div className='answer-container'>             
-                <button>{decode(questions[0])}</button>
-                <button>{decode(questions[1])}</button>
-                <button>{decode(questions[2])}</button>
-                <button>{decode(questions[3])}</button>
+                {answersElement}
             </div>
         </div>
     )
